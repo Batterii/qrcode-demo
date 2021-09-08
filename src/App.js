8588afa8fc34e6511692282c5583f89883c137c2
Noah Bogart <nbogart@batterii.com>
2021-09-08 16:29:25 -0400

    react-qrcode-logo

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { QRCode } from 'react-qrcode-logo';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,17 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <QRCode
+            value="https://vurvey.co"
+            logoImage="https://avatars.githubusercontent.com/u/603677?v=4"
+            size="200"
+            logoWidth="50"
+            quietZone="10"
+        />
       </header>
     </div>
   );
